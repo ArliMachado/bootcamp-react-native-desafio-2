2bb784d767436284a662a66ad33215d705e9b66a
Arli Machado <arli.machado@gmail.com>
2019-05-26 00:28:48 -0300

    Definindo rotas

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 import './config/DevToolsConfig';
 import './config/ReactotronConfig';
-
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import createNavigator from '~/routes';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-});
-const App = () => (
-  <View style={styles.container}>
-    <Text>Welcome to React Native!</Text>
-  </View>
-);
+const Routes = createNavigator();
+const App = () => <Routes />;
 
 export default App;
