993f3a9185b824077dde4110df8e23d48b707893
Arli Machado <arli.machado@gmail.com>
2019-05-26 12:16:02 -0300

    Definindo input para cadastro de novo repositorio

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
 
-import { View } from 'react-native';
+import { View, TextInput, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Header from '~/components/Header';
 import styles from './styles';
 
 export default class Repositories extends Component {
+  state = {
+    data: [],
+    repository: '',
+  };
+
   render() {
+    const { repository } = this.state;
     return (
       <View style={styles.container}>
         <Header title="Repositórios" />
+
+        <View style={styles.formContainer}>
+          <View style={styles.form}>
+            <TextInput
+              style={styles.input}
+              autoCapitalize="none"
+              autoCorrect={false}
+              placeholder="Adicionar novo repositório"
+              underlineColorAndroid="transparent"
+              value={repository}
+              onChangeText={text => this.setState({ repository: text })}
+            />
+            <TouchableOpacity onPress={() => {}}>
+              <Icon name="plus" size={24} style={styles.infoIcon} />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.lineStyle} />
+        </View>
       </View>
     );
   }
--- src/pages/Repositories/styles.js
+++ src/pages/Repositories/styles.js
@@ -1,10 +1,41 @@
 import { StyleSheet } from 'react-native';
-import { colors } from '~/styles';
+import { colors, metrics } from '~/styles';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.lighter,
+  },
+  formContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    paddingHorizontal: metrics.basePadding,
+    marginTop: metrics.baseMargin,
+  },
+  form: {
+    marginTop: metrics.baseMargin,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginRight: metrics.baseMargin,
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    backgroundColor: colors.white,
+    borderRadius: metrics.baseRadius,
+    height: 44,
+
+    // paddingHorizontal: metrics.basePadding,
+  },
+  infoIcon: {
+    color: colors.dark,
+    marginLeft: metrics.baseMargin,
+  },
+
+  lineStyle: {
+    marginTop: metrics.baseMargin * 2,
+    borderWidth: 0.5,
+    borderColor: colors.light,
   },
 });
 
