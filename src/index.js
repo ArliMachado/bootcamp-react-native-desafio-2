accb0decbba2b6d82e24720c62869ecc13ed5d13
Arli Machado <arli.machado@gmail.com>
2019-05-25 23:06:39 -0300

    Definindo estrutura inicial

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import './config/DevToolsConfig';
+import './config/ReactotronConfig';
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
+const App = () => (
+  <View style={styles.container}>
+    <Text>Welcome to React Native!</Text>
+  </View>
+);
+
+export default App;
