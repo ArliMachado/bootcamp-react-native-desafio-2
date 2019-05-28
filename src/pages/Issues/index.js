2e74703587896a60241784ba48593537fd137b33
Arli Machado <arli.machado@gmail.com>
2019-05-27 23:45:44 -0300

    Implementando a listagem de issues

--- src/pages/Issues/index.js
+++ src/pages/Issues/index.js
@@ -1,19 +1,69 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import { View } from 'react-native';
+import { View, FlatList, ActivityIndicator } from 'react-native';
 
 import Header from '~/components/Header';
 import styles from './styles';
+import api from '~/services/api';
+import Listitem from '~/components/ListItem';
 
 export default class Issues extends Component {
   state = {
     data: [],
+    refreshing: true,
+    loading: true,
+  };
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
+
+  componentWillMount() {
+    this.loadIssues();
+  }
+
+  loadIssues = async (filter = '') => {
+    const { navigation } = this.props;
+    const fullName = navigation.getParam('fullName');
+
+    const { data } = await api.get(`/repos/${fullName}/issues${filter}`);
+
+    const issues = data.map(issue => ({
+      id: issue.id,
+      title: issue.title,
+      subTitle: issue.user.login,
+      avatar_url: issue.user.avatar_url,
+    }));
+
+    this.setState({ data: issues, refreshing: false, loading: false });
+  };
+
+  renderListItem = ({ item }) => <Listitem data={item} navigateTo={() => {}} />;
+
+  renderList = () => {
+    const { data, refreshing } = this.state;
+    return (
+      <FlatList
+        data={data}
+        keyExtractor={item => String(item.id)}
+        renderItem={this.renderListItem}
+        onRefresh={this.loadIssues}
+        refreshing={refreshing}
+      />
+    );
   };
 
   render() {
+    const { loading } = this.state;
     return (
       <View style={styles.container}>
         <Header title="Issues" />
+        <View>
+          {loading ? <ActivityIndicator size={24} style={styles.loading} /> : this.renderList()}
+        </View>
       </View>
     );
   }
