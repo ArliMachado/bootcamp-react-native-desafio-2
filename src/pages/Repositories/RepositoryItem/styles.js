ed87ef1ade90c762d5e0f8a35ff3a002c788c577
Arli Machado <arli.machado@gmail.com>
2019-05-27 21:27:43 -0300

    Ajustando espaçamento entre a lista de repositorios

--- src/pages/Repositories/RepositoryItem/styles.js
+++ src/pages/Repositories/RepositoryItem/styles.js
@@ -8,7 +8,7 @@ const styles = StyleSheet.create({
     backgroundColor: colors.white,
     padding: metrics.basePadding,
     marginHorizontal: metrics.baseMargin * 2,
-    marginTop: metrics.baseMargin * 2,
+    marginTop: metrics.baseMargin,
   },
   imageContainer: {
     marginRight: metrics.baseMargin * 2,
